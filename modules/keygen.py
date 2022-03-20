7de5c556f7a7acbd31a06fea96cc441d912a337d
jjojas <justinjojas@gmail.com>
2022-03-20 22:17:10 +0700

    implement key generation and extraction techniques

--- modules/keygen.py
+++ modules/keygen.py
@@ -1,31 +1,92 @@
+from typing import Tuple
 
 def checkPrime(n : int) -> bool:
-    if (n == 1):
-        return False
-    i = 2
-    while (i*i <= n):
-        if (n%i ==0):
+    try:
+        if (n == 1):
             return False
-        i += 1
-    return True
+        i = 2
+        while (i*i <= n):
+            if (n%i ==0):
+                return False
+            i += 1
+        return True
+    except Exception as e:
+        raise e
 
-def genPrivateKey(p : int, q : int ,e : int) -> str:
+def relativePrime(a:int, b:int) -> bool:
+    try:
+        while (b):
+            a, b = b, a% b
+        return (a==1)
+    except Exception as e:
+        raise e
+
+def genKeys(p : int, q : int ,e : int) -> Tuple:
     '''
-    Membangkitkan kunci privat
+    Membangkitkan kunci publik dan privat
     '''
     d = 0
     if (checkPrime(p) and checkPrime(q)):
         n = p*q
         toit = (p-1)*(q-1)
-        d = 0.5
-        k = 1
-        while (d%1 != 0):
-            d = (1+k*toit)/e
-            k += 1
-        return (e,int(d))
+        if (relativePrime(toit,e)):
+            d = 0.5
+            k = 1
+            while (d%1 != 0):
+                d = (1+k*toit)/e
+                k += 1
+            return (int(d),n)
+        else:
+            raise Exception("E and Toitent not relatively prime. Choose another E!")
     else:
-        raise Exception("P or Q is not a prime number!")
+        raise Exception("P or Q is not a prime number. Pick another number(s)!")
+
+def saveKeys(name: str,e : int, d : int, n : int):
+    '''
+    Save generated keys to text file
+    '''
+    try:
+        f = open(f"key/{name}.pub","w")
+        pubKeystring = str(e) + '|' + str(n)
+        f.write(pubKeystring)
+        f.close()
+
+        g = open(f"key/{name}.pri","w")
+        privKeyString = str(d) + '|' + str(n)
+        g.write(privKeyString)
+        g.close()
+    except Exception as e:
+        raise e
+
+def createKeyFile(name:str,p:int,q:int,e:int):
+    try:
+        priv,pub = genKeys(p,q,e)
+        saveKeys(name,e,priv,pub)
+    except Exception as e:
+        raise e
+
+def openKeyFile(dir:str) -> Tuple:
+    try:
+        f = open(dir,"r")
+        content = f.read()
+        keytype = dir.split(".",10)[-1]
+        if (keytype == "pub" or keytype == "pri"):
+            if ("|" in content):
+                e,n = content.split("|",1)
+                f.close()
+                return(int(e),int(n))
+            else:
+                if (keytype == "pub"):
+                    raise Exception("Not a valid public key!")
+                else:
+                    raise Exception("Not a valid private key!")
+        else:
+            raise Exception("Not a valid key file!")
+    except Exception as e:
+        raise e
 
 if __name__ == "__main__":
-    print(genPrivateKey(47,71,79))
+    pass
+    # print(openKeyFile("../key/TestKey.pub"))
+    # createKeyFile("TestKey",47,71,79)
     # print(checkPrime(int(input())))
